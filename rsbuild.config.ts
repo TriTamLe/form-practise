fd1a841fd70cbf41ee7f850afd62ba9e0a556b00
TriTamLe <tritam.le54@gmail.com>
2024-01-23 19:28:40 +0700

    fix: remove redundant file and add html ìnormation

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,6 +3,12 @@ import { pluginReact } from '@rsbuild/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  html: {
+    title: 'Form Practise',
+    meta: {
+      description: 'A place to practise form in React',
+    },
+  },
   source: {
     alias: {
       '@': path.join(__dirname, 'src/'),
